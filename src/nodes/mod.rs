70ba8a0be127abcbf43d5858195730e816bd87c3
PhaestusFox <phaestusfox@gmail.com>
2023-07-07 13:50:51 +0800

    pub use scale node

--- src/nodes/mod.rs
+++ src/nodes/mod.rs
@@ -2,6 +2,7 @@ pub use index_node::IndexNode;
 pub use fps_node::FPSNode;
 pub use match_node::MatchNode;
 pub use script_node::ScriptNode;
+pub use scale_node::ScaleNode;
 
 pub mod variable_node;
 pub mod index_node;
